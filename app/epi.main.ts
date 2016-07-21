ca25786b1afebf7bdcc44e679520210110e76f83
Houdou <houdou.pt@gmail.com>
2016-07-22 03:12:13 +0800

    Initial Commit

---
+++ app/epi.main.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+	selector: "my-app",
+	template: `
+		<Button text="Hello World --Towa"></Button>
+	`
+})
+export class EpiMain {
+		
+}
---
+++ app/main.ts
@@ -0,0 +1,4 @@
+import {nativeScriptBootstrap} from "nativescript-angular/application";
+import {EpiMain} from "./epi.main";
+
+nativeScriptBootstrap(EpiMain);
